27d9df4b8635cbc401fac076ab3ff8e758ea59f1
Erick Suarez <esuarez@C02M621EFD58-esuarez.local>
2018-11-25 23:21:41 -0800

    Set up initial interface and action listeners for ATM application

---
+++ src/Atm.java
@@ -0,0 +1,186 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+public class Atm {
+
+	private JFrame frame;
+	private JTextField txtEnterAmount;
+	private JTextField txtFriendId;
+	private JTextField txtAccountId;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Atm window = new Atm("");
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Atm(String pin) {
+		initialize(pin);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize(String pin) {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 492, 321);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+				
+		JComboBox originAccountComboBox = new JComboBox();
+		originAccountComboBox.setBounds(386, 20, 52, 27);
+		frame.getContentPane().add(originAccountComboBox);
+		
+		txtEnterAmount = new JTextField();
+		txtEnterAmount.setText("Enter Amount");
+		txtEnterAmount.setBounds(148, 19, 130, 26);
+		frame.getContentPane().add(txtEnterAmount);
+		txtEnterAmount.setColumns(10);
+		
+		txtFriendId = new JTextField();
+		txtFriendId.setText("Friend ID");
+		txtFriendId.setBounds(148, 241, 130, 26);
+		frame.getContentPane().add(txtFriendId);
+		txtFriendId.setColumns(10);
+		
+		txtAccountId = new JTextField();
+		txtAccountId.setText("Account ID");
+		txtAccountId.setBounds(148, 165, 130, 26);
+		frame.getContentPane().add(txtAccountId);
+		txtAccountId.setColumns(10);
+		
+		JButton btnDeposit = new JButton("Deposit");
+		btnDeposit.setBounds(6, 19, 117, 29);
+		frame.getContentPane().add(btnDeposit);
+		
+		JButton btnTopup = new JButton("Top-Up");
+		btnTopup.setBounds(6, 113, 117, 29);
+		frame.getContentPane().add(btnTopup);
+		
+		JButton btnWithdrawal = new JButton("Withdrawal");
+		btnWithdrawal.setBounds(6, 52, 117, 29);
+		frame.getContentPane().add(btnWithdrawal);
+		
+		JButton btnPurchase = new JButton("Purchase");
+		btnPurchase.setBounds(6, 83, 117, 29);
+		frame.getContentPane().add(btnPurchase);
+		
+		JButton btnTransfer = new JButton("Transfer");
+		btnTransfer.setBounds(6, 165, 117, 29);
+		frame.getContentPane().add(btnTransfer);
+		
+		JButton btnWire = new JButton("Wire");
+		btnWire.setBounds(6, 194, 117, 29);
+		frame.getContentPane().add(btnWire);
+		
+		JButton btnPayFriend = new JButton("Pay Friend");
+		btnPayFriend.setBounds(6, 241, 117, 29);
+		frame.getContentPane().add(btnPayFriend);
+	
+		
+		// ==============================================
+		// Action Listeners
+		// ==============================================
+		String originAccount = (String)originAccountComboBox.getSelectedItem();
+        String totalAmount = txtEnterAmount.getText();
+        String friendID = txtFriendId.getText();
+        String targetAccount = txtAccountId.getText();
+        
+		btnDeposit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(Account.deposit(originAccount, totalAmount)) {
+					//TODO: on success
+				} else {
+					//TODO: on failure
+				}
+			}
+		});
+		
+		btnTopup.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(Account.topUp(originAccount, totalAmount)) {
+					//TODO: on success
+				} else {
+					//TODO: on failure
+				}
+			}
+		});
+		
+		btnWithdrawal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(Account.withdrawal(originAccount, totalAmount)) {
+					//TODO: on success
+				} else {
+					//TODO: on failure
+				}
+			}
+		});
+		
+		btnPurchase.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(Account.purchase(originAccount, totalAmount)) {
+					//TODO: on success
+				} else {
+					//TODO: on failure
+				}
+			}
+		});
+		
+		btnTransfer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(Account.transfer(originAccount, targetAccount, totalAmount)) {
+					//TODO: on success
+				} else {
+					//TODO: on failure
+				}
+			}
+		});
+		
+		btnWire.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(Account.wire(originAccount, targetAccount, totalAmount)) {
+					//TODO: on success
+				} else {
+					//TODO: on failure
+				}
+			}
+		});
+		
+		btnPayFriend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(Account.payFriend(originAccount, friendId, totalAmount)) {
+					//TODO: on success
+				} else {
+					//TODO: on failure
+				}
+			}
+		});
+	}
+}
---
+++ src/Authenticate.java
@@ -0,0 +1,90 @@
+import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Authenticate extends JFrame {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Authenticate window = new Authenticate();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Authenticate() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblPin = new JLabel("PIN:");
+		lblPin.setBounds(128, 102, 61, 16);
+		frame.getContentPane().add(lblPin);
+		
+		textField = new JTextField();
+		textField.setBounds(171, 97, 130, 26);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton authenticateButton = new JButton("Authenticate");
+		authenticateButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String pin = textField.getText();
+				
+				boolean authenticated = authenticatePin(pin);
+				
+			    if(authenticated) {
+			    	JOptionPane.showMessageDialog(frame, "Authentication was successful.");
+			    	Atm atm = new Atm(pin);
+			    	atm.main();
+			    	close();
+			    } else {
+			    	JOptionPane.showMessageDialog(frame, "Pin is invalid. Please try again or contact customer support.");
+			    }
+			}
+		});
+		authenticateButton.setBounds(159, 135, 117, 29);
+		frame.getContentPane().add(authenticateButton);
+
+		
+	}
+	
+	private void close() {
+		this.setVisible(false);
+		this.dispose();
+	}
+	
+	private boolean authenticatePin(String pin) {
+		//TODO: Implement
+		return false;
+	}
+}
