e461c796ddf660ad0b9686689a7f8d92038741ef
Zakary Bliss <zakarybliss@ucsb.edu>
2018-11-27 14:32:29 -0800

    Created an Account class

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,26 @@
 
 public class Account {
-
+	private int mID; 
+	private double mBalance; 
+	private boolean mStatus; 
+	
+	public Account(double balance) {
+		//Get ID from database 
+		mBalance = balance; 
+		mStatus = true; 
+	}
+	
+	public int getID() { return mID; }
+	
+	public double getBalance() { return mBalance; } 
+	
+	public boolean getStatus() { return mStatus; }
+	
+	public void setBalance(double balance) {
+		mBalance = balance; 
+	}
+	
+	public void setStatus(boolean status) {
+		mStatus = status; 
+	}
 }
